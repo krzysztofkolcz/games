31248c00b999e1e0a10dab0d67858199049bf693
Krzysztof Kołcz <krzysztof.kolcz@gmail.com>
2017-01-31 20:09:09 +0100

    circle moving on sinus line - keyadapter

---
+++ src/main/com/platformer01/threadkey/Circle.java
@@ -0,0 +1,68 @@
+package com.platformer01.threadkey;
+
+import java.awt.event.KeyEvent;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Circle{
+
+
+    private final int B_WIDTH = 640;
+    private final int B_HEIGHT = 480;
+
+    private int dx;
+    private int dy;
+    private int r;
+
+    private int cx;
+    private int cy;
+
+
+
+    public Circle(){ }
+    
+    double f(double x) {
+          return Math.sin(x);
+    }
+
+    public void move() {
+				cx += dx;
+				cy = (int)(f(cx/100.0)*100 + 240);
+				if(cx>B_WIDTH){
+					cx=0;
+				}
+    }
+
+    public void draw(Graphics g){
+				g.setColor(Color.RED);
+				g.fillOval(cx-10,cy-10,20,20);
+    }
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = -3; 
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 3;
+        }
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+        
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+
+    }
+}
+
