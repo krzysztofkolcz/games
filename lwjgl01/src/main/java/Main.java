579bac866e94353e43a000aa92c2e0f42edc2ebf
Krzysztof Kołcz <krzysztof.kolcz@gmail.com>
2017-09-03 17:09:35 +0200

    Flappy - new lwjgl part 1, youtube - 22 min.

--- lwjgl01/src/main/java/Main.java
+++ lwjgl01/src/main/java/Main.java
@@ -1,9 +1,24 @@
+/*  */
+/* import static org.lwjgl.glfw.GLFW.*; */
+/* import static org.lwjgl.opengl.GL11.*; */
+/* import static org.lwjgl.system.MemoryUtil.*; */
+/*  */
+/* import static org.lwjgl.system.MemoryStack.*; */
+/*  */
+/* import java.nio.ByteBuffer; */
 
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
-
-import java.nio.ByteBuffer;
 
 public class Main implements Runnable{
 
@@ -17,7 +32,7 @@ public class Main implements Runnable{
   private boolean running = false;
 
   public void start(){
-    running = false;
+    running = true;
     thread = new Thread(this, "Game");
     thread.start(); 
   }
@@ -29,21 +44,42 @@ public class Main implements Runnable{
 
     } 
 
+    // Configure GLFW
+    glfwDefaultWindowHints(); // optional, the current window hints are already the default
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
     glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+    // Create the window
     window = glfwCreateWindow(width, height, "Flappy", NULL, NULL);
+    if ( window == NULL )
+      throw new RuntimeException("Failed to create the GLFW window");
 
-		/* ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor()); */
-		/* glfwSetWindowPos(window, (GLFWvidmode.width(vidmode) - width) / 2, (GLFWvidmode.height(vidmode) - height) / 2); */
+    // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+    glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+      if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+        glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+    });
 
-    // Get the resolution of the primary monitor
-    GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    try ( MemoryStack stack = stackPush() ) {
+      IntBuffer pWidth = stack.mallocInt(1); // int* // ADDED
+      IntBuffer pHeight = stack.mallocInt(1); // int* //ADDED
 
-    // Center the window
-    glfwSetWindowPos(
-      window,
-      (vidmode.width() - pWidth.get(0)) / 2,
-      (vidmode.height() - pHeight.get(0)) / 2
-    );
+      // Get the window size passed to glfwCreateWindow
+      glfwGetWindowSize(window, pWidth, pHeight);
+
+      GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+      glfwSetWindowPos(
+        window,
+        (vidmode.width() - pWidth.get(0)) / 2, //GLFWvidmode -> vidmode
+        (vidmode.height() - pHeight.get(0)) / 2 //GLFWvidmode -> vidmode
+      );
+    }
+
+    // Make the OpenGL context current
+    glfwMakeContextCurrent(window);
+    // Make the window visible
+    glfwShowWindow(window);
   }
 
   public void run(){
@@ -51,15 +87,19 @@ public class Main implements Runnable{
     while(running){
       update();
       render(); 
+
+      if( glfwWindowShouldClose(window) ) {
+        running = false;
+      }
     } 
   }
 
   private void render(){
-
+      glfwSwapBuffers(window); // swap the color buffers
   }
 
   private void update(){
-
+      glfwPollEvents();
   }
 
 
